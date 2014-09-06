7ad14312dc1114eb899390d6b22c72299d10a946
Iktwo Sh <elmaildeliktwo@gmail.com>
2014-09-05 20:30:37 -0700

    Remove dpi calls from UIValues

--- src/uivalues.cpp
+++ src/uivalues.cpp
@@ -11,23 +11,15 @@
 
 UIValues::UIValues(QObject *parent) :
     QObject(parent),
-    m_dpi(0),
     m_isTablet(false)
 {
     QSettings settings;
     m_firstRun = settings.value("firstRun", true).toBool();
 
 #ifdef Q_OS_ANDROID
-    m_dpi = QAndroidJniObject::callStaticMethod<jint>("com/iktwo/musicgear/MusicGear",
-                                                      "getDPI", "()I");
-
     m_isTablet = QAndroidJniObject::callStaticMethod<jboolean>("com/iktwo/musicgear/MusicGear",
                                                                "isTablet", "()Z");
-#else
-    m_dpi = QGuiApplication::primaryScreen()->physicalDotsPerInch();
 #endif
-
-    m_dpMultiplier = (float) m_dpi / 160;
 }
 
 void UIValues::showMessage(const QString &message)
@@ -42,24 +34,6 @@ void UIValues::showMessage(const QString &message)
 #endif
 }
 
-QString UIValues::getBestIconSize(int height) const
-{
-    if (height < 42)
-        return "ldpi/";
-    else if (height < 60)
-        return "mdpi/";
-    else if (height < 84)
-        return "hdpi/";
-    else if (height < 120)
-        return "xhdpi/";
-    else if (height < 168)
-        return "xxhdpi/";
-    else if (height < 216)
-        return "xxxhdpi/";
-    else
-        return "";
-}
-
 bool UIValues::firstRun() const
 {
     return m_firstRun;
@@ -72,36 +46,6 @@ void UIValues::setFirstRun(bool firstRun)
 
     m_firstRun = firstRun;
     emit firstRunChanged();
-}
-
-int UIValues::dpi() const
-{
-    return m_dpi;
-}
-
-void UIValues::setDpi(int dpi)
-{
-    if (m_dpi == dpi)
-        return;
-
-    m_dpi = dpi;
-    emit dpiChanged();
-
-    setDpMultiplier((float) m_dpi / 160);
-}
-
-float UIValues::dpMultiplier() const
-{
-    return m_dpMultiplier;
-}
-
-void UIValues::setDpMultiplier(float dpMultiplier)
-{
-    if (m_dpMultiplier == dpMultiplier)
-        return;
-
-    m_dpMultiplier = dpMultiplier;
-    emit dpMultiplierChanged();
 }
 
 bool UIValues::isTablet() const
