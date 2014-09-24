9937496d781ca9b47821835c8179c53cc1ec4613
Iktwo Sh <elmaildeliktwo@gmail.com>
2014-09-23 21:51:28 -0700

    Updated translations

--- translations/translation_es.ts
+++ translations/translation_es.ts
@@ -4,57 +4,62 @@
 <context>
     <name>AboutPage</name>
     <message>
-        <location filename="qml/AboutPage.qml" line="69"/>
+        <location filename="../qml/AboutPage.qml" line="75"/>
         <source>Stream your favorite music from &lt;a href=&quot;http://goear.com&quot;&gt;Goear&lt;/a&gt;</source>
         <translation>Escucha tu musica favorita de &lt;a href=\&quot;http://goear.com\&quot;&gt;http://goear.com&lt;/a&gt;</translation>
     </message>
     <message>
-        <location filename="qml/AboutPage.qml" line="79"/>
+        <location filename="../qml/AboutPage.qml" line="91"/>
         <source>Icons under &lt;a href=&quot;http://creativecommons.org/licenses/by/3.0/legalcode&quot;&gt;CC 3.0&lt;/a&gt; by &lt;a href=&quot;http://www.freepik.com/&quot;&gt;Freepik&lt;/a&gt;</source>
         <translation>Iconos bajo  &lt;a href=&quot;http://creativecommons.org/licenses/by/3.0/legalcode&quot;&gt;CC 3.0&lt;/a&gt; por &lt;a href=&quot;http://www.freepik.com/&quot;&gt;Freepik&lt;/a&gt;</translation>
     </message>
     <message>
-        <location filename="qml/AboutPage.qml" line="89"/>
+        <location filename="../qml/AboutPage.qml" line="107"/>
         <source>Built on</source>
         <translation>Compilado en</translation>
     </message>
     <message>
-        <location filename="qml/AboutPage.qml" line="104"/>
+        <location filename="../qml/AboutPage.qml" line="128"/>
         <source>Source code is available at &lt;a href=&quot;http://github.com/iktwo/musicgear&quot;&gt;Github&lt;/a&gt;</source>
         <translation>Código fuente disponible en &lt;a href=\&quot;http://github.com/iktwo/musicgear\&quot;&gt;Github&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="150"/>
+        <source>Muli font by Vernon Adams &lt;a href=&quot;http://scripts.sil.org/OFL&quot;&gt;SIL Open Font License, 1.1&lt;/a&gt;</source>
+        <translation>Fuente Muli for Vernon Adams &lt;a href=&quot;http://scripts.sil.org/OFL&quot;&gt;SIL Open Font License, 1.1&lt;/a&gt;</translation>
     </message>
 </context>
 <context>
     <name>FakeMainPage</name>
     <message>
-        <location filename="qml/FakeMainPage.qml" line="40"/>
+        <location filename="../qml/FakeMainPage.qml" line="41"/>
         <source>Menu</source>
         <translation>Menu</translation>
     </message>
     <message>
-        <location filename="qml/FakeMainPage.qml" line="43"/>
+        <location filename="../qml/FakeMainPage.qml" line="44"/>
         <source>Share</source>
         <translation>Compartir</translation>
     </message>
     <message>
-        <location filename="qml/FakeMainPage.qml" line="128"/>
-        <location filename="qml/FakeMainPage.qml" line="212"/>
+        <location filename="../qml/FakeMainPage.qml" line="133"/>
+        <location filename="../qml/FakeMainPage.qml" line="222"/>
         <source>This is your playlist. The music that you add will appear here. Also when you play music the media controls will appear at the bottom.</source>
         <translation>Está es tu lista de reproducción. La música que agregues aparecera aquí. Cuando reproduces tu música los controles apareceran en la parte de abajo.</translation>
     </message>
     <message>
-        <location filename="qml/FakeMainPage.qml" line="143"/>
-        <location filename="qml/FakeMainPage.qml" line="227"/>
+        <location filename="../qml/FakeMainPage.qml" line="148"/>
+        <location filename="../qml/FakeMainPage.qml" line="237"/>
         <source>Done</source>
         <translation>Listo</translation>
     </message>
     <message>
-        <location filename="qml/FakeMainPage.qml" line="259"/>
+        <location filename="../qml/FakeMainPage.qml" line="270"/>
         <source>Now press and hold a song, you will see the song menu, you can share your favorite songs there. This works while searching for music too!</source>
         <translation>Presiona y manten presionada una canción, verás un menu, puedes compartir tus canciones favoritas desde ahí. Esto también funciona cuando estas buscando música!</translation>
     </message>
     <message>
-        <location filename="qml/FakeMainPage.qml" line="268"/>
+        <location filename="../qml/FakeMainPage.qml" line="279"/>
         <source>This is the search button, press it!</source>
         <translation>Este es el botón para buscar, presionalo!</translation>
     </message>
@@ -62,60 +67,82 @@
 <context>
     <name>FakeSearchPage</name>
     <message>
-        <location filename="qml/FakeSearchPage.qml" line="42"/>
+        <location filename="../qml/FakeSearchPage.qml" line="42"/>
         <source>Search songs and artists</source>
         <translation>Busca canciones o artistas</translation>
     </message>
     <message>
-        <location filename="qml/FakeSearchPage.qml" line="105"/>
+        <location filename="../qml/FakeSearchPage.qml" line="108"/>
         <source>Menu</source>
         <translation>Menu</translation>
     </message>
     <message>
-        <location filename="qml/FakeSearchPage.qml" line="108"/>
+        <location filename="../qml/FakeSearchPage.qml" line="111"/>
         <source>Share</source>
         <translation>Compartir</translation>
     </message>
     <message>
-        <location filename="qml/FakeSearchPage.qml" line="275"/>
         <source>Downloading</source>
-        <translation>Descargando</translation>
+        <translation type="vanished">Descargando</translation>
     </message>
 </context>
 <context>
     <name>MainPage</name>
     <message>
-        <location filename="qml/MainPage.qml" line="52"/>
+        <location filename="../qml/MainPage.qml" line="54"/>
         <source>Menu</source>
         <translation>Menu</translation>
     </message>
     <message>
-        <location filename="qml/MainPage.qml" line="55"/>
+        <location filename="../qml/MainPage.qml" line="57"/>
         <source>Share</source>
         <translation>Compartir</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="128"/>
+        <source>You have no music on the playlist</source>
+        <translation>No has agregado musica al playlist</translation>
     </message>
 </context>
 <context>
     <name>SearchPage</name>
     <message>
-        <location filename="qml/SearchPage.qml" line="40"/>
+        <location filename="../qml/SearchPage.qml" line="48"/>
         <source>Search songs and artists</source>
         <translation>Busca canciones o artistas</translation>
     </message>
     <message>
-        <location filename="qml/SearchPage.qml" line="103"/>
+        <location filename="../qml/SearchPage.qml" line="115"/>
         <source>Menu</source>
         <translation>Menu</translation>
     </message>
     <message>
-        <location filename="qml/SearchPage.qml" line="106"/>
+        <location filename="../qml/SearchPage.qml" line="118"/>
         <source>Share</source>
         <translation>Compartir</translation>
     </message>
     <message>
-        <location filename="qml/SearchPage.qml" line="274"/>
+        <location filename="../qml/SearchPage.qml" line="267"/>
         <source>Downloading</source>
         <translation>Descargando</translation>
+    </message>
+    <message>
+        <location filename="../qml/SearchPage.qml" line="346"/>
+        <source>No results</source>
+        <translation>No hay resultados</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="217"/>
+        <source>Update</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="219"/>
+        <source>There&apos;s a new update available, you are running version %1 and you can update to version %2.</source>
+        <translation>Hay una nueva actualización disponible, tienes la versión %1 y puedes actualizar a la versión %2.</translation>
     </message>
 </context>
 </TS>
